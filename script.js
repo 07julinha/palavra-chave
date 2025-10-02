7c36bb40610c90660e5bc405db8098c367cf744a
07julinha <julia.pereira07@escola.pr.gov.br>
2025-10-02 11:03:09 -0300

    .

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function mostraPalavrasChave() {
    const texto = document.querySelector('#entrada-de-texto').value;
    const campoResultado = document.querySelector('#resultado-palavrachave');
    const palavrasChave = processaTexto(texto);
-campoResultado.textContent = palavrasChave.join(", ");
+
+
+   campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto) {
+unction processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
     let frequencias = [];
@@ -21,5 +23,32 @@ for (let i of palavras) {
       }
     }
    }
+console.log(frequencias);
 
-   console.log(frequencias)
+return palavras;
+
+
+function contaFrequencias(palavras) 
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+         }
+    }
+      return palavras;
+   }
+      
+   function tiraPalavrasRuins(palavras) {
+       const palavras_ruins = ["para", "uma"," nós"];
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has.(palavras) && palavras.length.> 2 {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
